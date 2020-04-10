93ddc847cf9c4ed41c810efbd2465679e9333ff9
irahulcse <irahulcse@users.noreply.github.com>
2020-04-10 17:11:53 +0000

    deploy: ac10f82f6d7a61c5b539f66aa606fc5fc3abf357

---
+++ flutter_service_worker.js
@@ -0,0 +1,40 @@
+'use strict';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "/favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"/manifest.json": "259b65cac04f89b05a771fd7344a068e",
+"/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"/assets/AssetManifest.json": "bda075a72c5ca0347c9ae77f3a2b08fb",
+"/assets/LICENSE": "964211db6a8b173b1744e68da77ce459",
+"/assets/assets/rahul_new.jpg": "7df8349565b7f0cf6cd8948fe925d307",
+"/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"/assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"/main.dart.js": "1e68fa8b2d912bb62c7e27aab5d14651",
+"/index.html": "ecd83f1c9bf1b46ab799b3ab899dc433"
+};
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheName) {
+      return caches.delete(cacheName);
+    }).then(function (_) {
+      return caches.open(CACHE_NAME);
+    }).then(function (cache) {
+      return cache.addAll(Object.keys(RESOURCES));
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
